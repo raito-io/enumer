115c804757c49bea0126d6f4f34d7e5eeedde663
Ruben Mennes <ruben.mennes@me.com>
2025-01-30 14:16:09 +0100

    Update linting

--- stringer.go
+++ stringer.go
@@ -245,7 +245,7 @@ type Package struct {
 // parsePackage exits if there is an error.
 func (g *Generator) parsePackage(patterns []string) {
 	cfg := &packages.Config{
-		Mode: packages.LoadSyntax, //nolint
+		Mode: packages.LoadSyntax,
 		// TODO: Need to think about constants in test files. Maybe write type_string_test.go
 		// in a separate pass? For later.
 		Tests: false,
